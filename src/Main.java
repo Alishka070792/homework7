f06bb09d399f4691addbe2213a3109996243a41a
Alina <gazievaalina070792@gmail.com>
2026-09-19 15:10:06 +0300

    task 1 is done

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
+        //Задача 1
+        System.out.println("Task 1");
+        int firstFriday = 2;
+        int currentDay = 1;
+        for (; currentDay <= 31; currentDay++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            if ((currentDay - firstFriday) % 7 == 0) {
+                System.out.println("Сегодня пятница, " + currentDay + "-е число. Необходимо подготовить отчет.");
+            }
         }
+        System.out.println();
+        //Задача 2
+        System.out.println("Task 2");
+
     }
 }
