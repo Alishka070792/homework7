5a9c25079acfe132d2b96f8632b8550150009129
Alina <gazievaalina070792@gmail.com>
2026-09-19 15:41:09 +0300

    task 1, 2 are done

--- src/Main.java
+++ src/Main.java
@@ -15,8 +15,28 @@ public class Main {
             }
         }
         System.out.println();
-        //Задача 2
-        System.out.println("Task 2");
+        //Задача 2.1
+        System.out.println("Task 2.1");
+        int totalDistance = 42195;
+        int fixedDistance = 500;
+        int passedDistance = 0;
+        do {
+            System.out.println("Держитесь! Осталось " + totalDistance + " метров.");
+            totalDistance = totalDistance - fixedDistance;
+            passedDistance = passedDistance + fixedDistance;
+        } while (totalDistance > 0);
+        System.out.println();
+//Задача 2.2
+        System.out.println("Task 2.2");
+        int myDistance = 0;
+        int fullDistance = 42195;
+        for (; myDistance < fullDistance; myDistance += 500) {
+            System.out.println("Держитесь! Осталось " + (fullDistance - myDistance) + " метров.");
+        }
+        System.out.println();
+
+        //Задача 3
+        System.out.println("Task 3");
 
     }
 }
